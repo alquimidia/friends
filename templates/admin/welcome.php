1b855757b30d1232a3aa405bd04756da6966c036
Lívia Gouvêa <livia@mail.mayfirst.org>
2024-10-18 19:02:28 -0300

    Correções após merge de branch Fedipress na main

--- templates/admin/welcome.php
+++ templates/admin/welcome.php
@@ -10,5 +10,5 @@ $first_friend = array(
 	'display_name' => sprintf( __( 'Add %s, creator of the Friends plugin, as a friend now', 'friends' ), 'Alex Kirk' ),
 	'url'          => 'https://alex.kirk.at/',
 );
-
+?>
 <iframe id="iframePageFriends" width="100%" src="friends/" frameborder="0"  scrolling="auto" onload="resizeIframe(this)"></iframe>
